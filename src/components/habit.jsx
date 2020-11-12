f6b4e2572d4f0bcefe0a2870a03b4c95dccd9ab1
carrie9497 <67547279+carrie9497@users.noreply.github.com>
2020-11-12 23:26:09 +0900

    habit list

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+    state = {
+        count: 0,
+    };
+
+    handleIncrement = () => {
+        this.setState({count: this.state.count + 1});
+    };
+
+    handleDecrement = () => {
+
+        const count = this.state.count -1;
+        this.setState({count: count < 0 ? 0 : count});
+    };
+
     render() {
+        
+        const {name, count} = this.props.habit;
         return (
             <li className="habit">
-                <span className="habit-name">Reading</span>
-                <span className="habit-count">3</span>
-                <button className="habit-button habit-increase">
+                <span className="habit-name">{name}</span>
+                <span className="habit-count">{count}</span>
+                <button className="habit-button habit-increase" onClick={this.handleIncrement}>
                     <i class="fas fa-plus-square"></i>
                 </button>
-                <button className="habit-button habit-decrease">
+                <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
                     <i class="fas fa-minus-square"></i>
                 </button>
                 <button className="habit-button habit-delete">
